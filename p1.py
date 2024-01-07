de0780f92de571f6cee6dd4fd15b27b485744a45
shraddha <shraddhaaher2001@gmail.com>
2024-01-07 17:07:43 +0530

    practising coding ninja problem

---
+++ p1.py
@@ -0,0 +1,38 @@
+#Your code goes here
+
+# name=input()
+# age=int(input())
+# print('The name of the person is ',name,end=" ")
+# print('and the age is ',age)
+
+# while 1:
+#     i=int(input())
+#     if i==5:
+#         break
+
+# n=12345
+
+# for i in n:
+#     print(i)
+num=0 
+factorial=1 
+for i in range(num,0,-1):
+    factorial=factorial*i
+   
+print(factorial)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
